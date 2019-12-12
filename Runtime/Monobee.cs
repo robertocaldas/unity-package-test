63beaa265fce077461852f3deced0d79647df18e
Roberto Caldas <roberto.caldas@mytona.com>
2019-12-12 17:12:49 +1300

    Mirror test 3

--- Runtime/Monobee.cs
+++ Runtime/Monobee.cs
@@ -7,7 +7,7 @@ public class Monobee : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogError("something here..........");
+        Debug.LogError("something here..............");
     }
 
     // Update is called once per frame
